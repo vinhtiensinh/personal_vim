ae0a90db4a7d423588eabd5de6eb59f351590bb6
vtran <vinh.tran@rea-group.com>
2010-10-03 20:10:45 +1100

    basic function finish for command-list

--- plugin/command-list.vim
+++ plugin/command-list.vim
@@ -1,4 +1,8 @@
 rubyf $HOME/.vim/ruby/command-list/ruby_vim_extension.rb
 rubyf $HOME/.vim/ruby/command-list/show_list.rb
-ruby VIM::register_vim_command('DisplayCommandList', 'display_command_list')
-ruby VIM::register_vim_command('ExecuteFunction', 'execute_function')
+ruby VIM::register_vim_command('DisplayCommandList', 'CommandList::display_command_list')
+ruby VIM::register_vim_command('ExecuteFunction', 'CommandList::execute_function')
+ruby CommandList::register('Cucumber', 'StepFileRecommendations', 'display step files')
+ruby CommandList::register('Cucumber', 'JumpStep', 'jump to the definition of the step')
+ruby CommandList::register('Cucumber', 'CheckLineStepDefinition', 'check step definition of current line')
+ruby CommandList::register('Cucumber', 'CheckFileStepDefinition', 'check step definition of the file')
--- ruby/command-list/show_list.rb
+++ ruby/command-list/show_list.rb
@@ -1,14 +1,51 @@
-def display_command_list()
-  VIM::command('split __Command-List__')
-  command_list = ["StepFileRecommendations", "JumpStep"]
-  VIM::Buffer.current.append(0, command_list)
-  VIM::set_option('buftype=nofile')
-  VIM::command('map <buffer> <CR> :call ExecuteFunction()<CR>')
+class CommandList
+  @@lists = {}
+  def self.display_command_list()
+    VIM::command('split __Command-List__')
+    VIM::Buffer.current.append(0, @@lists.values.join("\n"))
+    VIM::set_option('buftype=nofile')
+    VIM::command('map <buffer> <CR> :call ExecuteFunction()<CR>')
+  end
+
+  def self.execute_function()
+    function = VIM::Buffer.current.line.match(/\s(\w+)\s*$/).captures.first
+    VIM::command("bd __Command-List__")
+    VIM::command("call #{function}()")
+  end
+
+  def self.register (menu, function, text, description = '')
+    menu = menu || 'Miscellaneous'
+    @@lists[menu] = Menu.new(menu) unless @@lists.has_key?(menu)
+    @@lists[menu].append(MenuItem.new(function, text, description))
+  end
+ end
+class MenuItem
+  attr_reader :function, :text, :description
+  def initialize function, text, description
+    @function    = function
+    @text        = text
+    @description = description
+  end
+
+  def to_s
+    sprintf("%s ...  %s", @text, @function)
+  end
 end
 
-def execute_function()
-  function = VIM::Buffer.current.line.match(/^\s*(\w+)?/).captures.first
-  VIM::command("bd __Command-List__")
-  VIM::command("call #{function}()")
+class Menu
+  attr_reader :items, :name
+
+  def initialize name
+    @name  = name
+    @items = []
+  end
+
+  def append items
+    @items.push(items)
+  end
+
+  def to_s
+    "#{@name}\n\t" + @items.join("\n\t")
+  end
 end
 
