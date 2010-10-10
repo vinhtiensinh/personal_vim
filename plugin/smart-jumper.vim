0f1655022b9787b374abd346427fa32e0bc94a36
Vinh Tran <vinhtiensinh@yahoo.com>
2010-10-10 20:26:47 +1100

    update smart jumber

--- plugin/smart-jumper.vim
+++ plugin/smart-jumper.vim
@@ -3,6 +3,7 @@ ruby << EOF
   VIM::evaluate('&runtimepath').to_s.split(',').each do |path|
     if File.exist?("#{path}/ruby/smart-jumper")
       require "#{path}/ruby/smart-jumper/smart_jumper.rb"
+      require "#{path}/plugin/smart-jumper.rb"
     end
   end
 EOF
--- ruby/smart-jumper/smart_jumper.rb
+++ ruby/smart-jumper/smart_jumper.rb
@@ -1,35 +1,73 @@
 class SmartJumper
-  def self.jump string
-    get_jumper.jump string
+
+  @@jumbers = []
+
+  def self.jump string=nil
+    jumper = self.get_jumper
+    
+    if jumper.nil?
+      VIM::command("echo 'No jumber find'")
+    else
+      file_path = jumper.jump string
+    end
+
+    VIM::command("tabnew #{file_path}")
+
+  end
+
+  def self.jump_with id, string
+    jumper = self.get_jumper_with_id(id)
+    
+    if jumper.nil?
+      VIM::command("echo 'No jumber with id #{id} found'")
+    else
+      jumper.jump string
+    end
+
   end
 
   def self.get_jumper
-    PerlJumper.new(:paths => ['/Users/vinh_tran/branches/librea/lib/'])
+    @@jumbers.each do | jumper |
+      return jumper if jumper.match
+    end
   end
 
-  def self.register_jumper options
+  def self.register_jumper jumper
+    @@jumbers.push(jumper)
   end
 end
 
 class PerlJumper
   
-  attr_reader :paths
+  attr_reader :paths, :files, :id
 
   def initialize options
     @paths = options[:paths]
+    @files = options[:files].split(',')
+    @id    = options[:id]
+  end
+
+  def match
+    buffer_name = VIM::Buffer.current.name
+    @files.each do | file |
+      return true if buffer_name.match(file)
+    end
+
+    return false
+
   end
 
   def jump string
+    string = string || VIM::evaluate("expand('<cword>')")
+
     file_path = string.gsub!('::', '/')
     file_path.sub!(/\s*$/, '.pm')
 
     @paths.each do | path |
       if File.exists?(path + file_path)
-        file_path = path + file_path
-        break
+        return path + file_path
       end
     end
 
-    VIM::command("tabnew #{file_path}")
   end
 end
