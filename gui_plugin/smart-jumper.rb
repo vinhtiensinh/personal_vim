a9e4fd49494a6c99fc1326379bb4174011e18180
vtran <vinh.tran@rea-group.com>
2011-01-17 21:13:49 +1100

    add smart-jumper change

--- gui_plugin/smart-jumper.rb
+++ gui_plugin/smart-jumper.rb
@@ -1,16 +1,16 @@
 SmartJumper.register_jumper(
   PerlJumper.new(
   :paths => [
-    '/Users/vinh_tran/branches/master/librea/lib/',
-    '/Users/vinh_tran/branches/master/agentdesktop/lib/',
-    '/Users/vinh_tran/branches/master/reaxml/lib/',
-    '/Users/vinh_tran/branches/master/accpac/lib/',
-    '/Users/vinh_tran/branches/master/appcmd/lib/',
-    '/web/home/vtran/branches/master/librea/lib/',
-    '/web/home/vtran/branches/master/agentdesktop/lib/',
-    '/web/home/vtran/branches/master/reaxml/lib/',
-    '/web/home/vtran/branches/master/accpac/lib/',
-    '/web/home/vtran/branches/master/appcmd/lib/',
+    '/Users/vinh_tran/ad-dev01/branches/master/librea/lib/',
+    '/Users/vinh_tran/ad-dev01/branches/master/agentdesktop/lib/',
+    '/Users/vinh_tran/ad-dev01/branches/master/reaxml/lib/',
+    '/Users/vinh_tran/ad-dev01/branches/master/accpac/lib/',
+    '/Users/vinh_tran/ad-dev01/branches/master/appcmd/lib/',
+    '/web/home/vtran/ad-dev01/branches/master/librea/lib/',
+    '/web/home/vtran/ad-dev01/branches/master/agentdesktop/lib/',
+    '/web/home/vtran/ad-dev01/branches/master/reaxml/lib/',
+    '/web/home/vtran/ad-dev01/branches/master/accpac/lib/',
+    '/web/home/vtran/ad-dev01/branches/master/appcmd/lib/',
   ],
   :files => '.pm,.t,.pl',
   :id    => 'perl'
